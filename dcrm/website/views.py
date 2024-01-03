d4ca2c39be56f371fc7cea31f27ffab1d6c5ed9b
kwabenahcode <mrofori507@gmail.com>
2024-01-03 21:22:25 +0000

    changes made

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 def home(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
     return render(request, 'home.html', {})
 
 def login_user(request):
