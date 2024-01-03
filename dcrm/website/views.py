afb4d44b898121d78eace420c6343cfe731f7104
kwabenahcode <mrofori507@gmail.com>
2024-01-03 22:04:34 +0000

    authenticating user login

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
@@ -6,10 +6,16 @@ def home(request):
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
+        #authenticate user
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have successfully logged in!! ")
+            return redirect('home')
+        else:
+            messages.error(request, "There was an error logging in!...Try again")
     return render(request, 'home.html', {})
 
-def login_user(request):
-    pass
 
 def logout_user(request):
     pass
